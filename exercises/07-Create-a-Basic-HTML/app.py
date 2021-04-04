9d95091203565375442fdb8c677f09ab65bdc69b
byronvillalobos <byronvillalobos@hotmail.com>
2021-04-04 20:24:07 +0000

    comments

--- exercises/07-Create-a-Basic-HTML/app.py
+++ exercises/07-Create-a-Basic-HTML/app.py
@@ -6,7 +6,8 @@ e = '<html>'
 f = '</head>'
 g = '<title>'
 h = '<body>'
-
 # ⬆ DON'T CHANGE THE CODE ABOVE ⬆
 # ↓ start coding below here ↓
 
+html_document = e + c + g + a + f + h + d + b
+print(html_document)
