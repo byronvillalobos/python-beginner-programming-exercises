74e0985efa772a0077bced68b8f3c8b0b57e17ff
byronvillalobos <byronvillalobos@hotmail.com>
2021-04-04 19:42:12 +0000

    comments

--- exercises/04-Multiply-Two-Values/app.py
+++ exercises/04-Multiply-Two-Values/app.py
@@ -1,5 +1,5 @@
 # your code here
 
 
-variables_are_cool = 2345  + 7323 
+variables_are_cool = 2345  * 7323 
 print(variables_are_cool)
--- exercises/06-String-Concatenation/app.py
+++ exercises/06-String-Concatenation/app.py
@@ -1,5 +1,6 @@
 # Set the values for my_var1 and my_var2 here
-
+my_var1 = 'a'
+my_var2 = 'b'
 
 ## Don't change below this line
 the_new_string = my_var1+' '+my_var2
